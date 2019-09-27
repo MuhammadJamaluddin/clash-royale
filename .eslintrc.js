aece4840ee713f29b234a7c4b1b2a6c225878cdc
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-09-28 00:03:38 +0200

    Include the core dependencies and files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": "airbnb",
+  rules: {
+    "linebreak-style": ["error", "windows"],
+    "no-console": "off",
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
+    "react/react-in-jsx-scope": "off"
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "mocha": true,
+    "jest": true,
+  },
+  "globals": {
+    "React": "readonly",
+    "ReactDOM": "readonly",
+    "styled": "readonly",
+  },
+};
