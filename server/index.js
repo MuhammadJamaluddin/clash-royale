fd90ef441a5e61e6b1607508730c48247e3eb122
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-09-29 01:49:10 +0200

    Implement the route responsible for fetching the cards data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -6,3 +7,14 @@ const port = 3000;
 app.listen(port, () => console.log(`the app is listening on port ${port}`));
 
 app.use(express.static('public'));
+
+app.get('/cardsData', (req, res) => {
+  axios.get('http://www.clashapi.xyz/api/cards')
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
