89eb08600b36161f489d0e93a90c61dd858ec826
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-10-01 05:23:54 +0200

    Rename the action creator file

--- client/actions/cardsData.js
+++
@@ -1,6 +0,0 @@
-const changeCardsData = (cardsData) => ({
-  type: 'STORE_CARDS_DATA',
-  cardsData,
-});
-
-export default changeCardsData;
--- client/reducers/storeCardsData.js
+++ client/reducers/storeCardsData.js
@@ -1,4 +1,4 @@
-const storeCardsData = (previousState = [], action) => {
+const storeCardsData = (previousState = [], action = {}) => {
   switch (action.type) {
     case 'STORE_CARDS_DATA':
       return action.cardsData;
