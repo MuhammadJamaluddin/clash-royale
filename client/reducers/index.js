601b4d1443f99d3ffc1b62929f534ceedb43ce4f
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-10-01 05:21:56 +0200

    Include the generateRandomDeckReducer within the rootReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import cardsDataReducer from './storeCardsData';
+import generateRandomDeckReducer from './generateRandomDeck';
 
 const rootReducer = combineReducers({
   cardsData: cardsDataReducer,
+  currentDeck: generateRandomDeckReducer,
 });
 
 export default rootReducer;
