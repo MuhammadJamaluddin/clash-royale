d0ee28fc75330581d7821c9125fac43e1e6b2a30
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-10-01 06:24:43 +0200

    Refactor the DeckBuilder into a functional component

--- client/src/components/DeckBuilder.jsx
+++ client/src/components/DeckBuilder.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import Card from './Card';
+import RandomizeButton from './randomizeButton';
 import ShareButton from './ShareButton';
 
-class DeckBuilder extends React.Component {
-  constructor(props) {
-    super(props);
-    const { generateRandomDeck } = this.props;
-    const { cardsData } = this.props;
-    this.generateRandomDeck = generateRandomDeck;
-    this.cardsData = cardsData;
-  }
-
-  render() {
-    const { currentDeck } = this.props;
-
-    const deck = [];
-    for (let i = 0; i < 8; i += 1) {
-      if (currentDeck[i] === undefined) {
-        deck.push(<Card selected={false} key={i} />);
-      } else {
-        deck.push(<Card selected key={currentDeck[i].idName} card={currentDeck[i]} />);
-      }
+const DeckBuilder = ({ cardsData, currentDeck, generateRandomDeck}) => {
+  const deck = [];
+  for (let i = 0; i < 8; i += 1) {
+    if (currentDeck[i] === undefined) {
+      deck.push(<Card selected={false} key={i} />);
+    } else {
+      deck.push(<Card selected key={currentDeck[i].idName} card={currentDeck[i]} />);
     }
-
-    return (
-      <div>
-        <div id="deck">
-          {deck}
-        </div>
-        <ShareButton />
-      </div>
-    );
   }
-}
+
+  return (
+    <div>
+      <div id="deck">
+        {deck}
+      </div>
+      <RandomizeButton cardsData={cardsData} generateRandomDeck={generateRandomDeck} />
+      <ShareButton />
+    </div>
+  );
+};
 
 export default DeckBuilder;
