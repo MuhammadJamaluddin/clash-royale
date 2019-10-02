a06430597e2a9856e4ca7d0da497cbe55b45867c
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-10-02 15:37:06 +0200

    Fix an ESlint error

--- client/reducers/handleDeckState.js
+++ client/reducers/handleDeckState.js
@@ -4,10 +4,9 @@ const handleDeckState = (previousState = [], action = {}) => {
   switch (action.type) {
     case 'GENERATE_RANDOM_DECK':
       // Select random 8 elements from the array
-      return randomize(action.cardsData, 8).map((card) =>
-        ({ ...card, open: false, selectMode: false }));
+      return randomize(action.cardsData, 8).map((card) => (
+        { ...card, open: false, selectMode: false }));
     case 'REMOVE_CARD':
-      // removeCard
       return previousState.map((card) => (
         (card._id === action.cardId) ? { ...card, state: 'notSelected' } : card
       ));
